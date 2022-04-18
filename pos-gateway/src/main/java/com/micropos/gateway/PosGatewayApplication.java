f72e78cdc15141a83dfb8e71cf7c5d9a4d73aef3
CAR-hash <lejian663@126.com>
2022-04-18 16:49:54 +0800

    eureka

--- pos-gateway/src/main/java/com/micropos/gateway/PosGatewayApplication.java
+++ pos-gateway/src/main/java/com/micropos/gateway/PosGatewayApplication.java
@@ -1,4 +1,4 @@
-package com.micropos.cart;
+package com.micropos.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,8 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class CartApplication {
+public class PosGatewayApplication {
     public static void main(String[] args){
-        SpringApplication.run(CartApplication.class,args);
+        SpringApplication.run(PosGatewayApplication.class,args);
     }
 }
